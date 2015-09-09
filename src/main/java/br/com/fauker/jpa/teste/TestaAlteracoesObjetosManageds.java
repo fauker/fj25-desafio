2198120f5f8e2f552a1af71653315b91975089a5
Lucas Moreira <eskerlucas1@gmail.com>
2015-09-08 22:17:13 -0300

    testando atualizações indevidas com JPA.

---
+++ src/main/java/br/com/fauker/jpa/teste/TestaAlteracoesObjetosManageds.java
@@ -0,0 +1,29 @@
+package br.com.fauker.jpa.teste;
+
+import javax.persistence.EntityManager;
+
+import br.com.fauker.jpa.domain.Conta;
+import br.com.fauker.jpa.util.JPAUtil;
+
+public class TestaAlteracoesObjetosManageds {
+	public static void main(String[] args) {
+		EntityManager manager = new JPAUtil().getEntityManager();
+		
+		manager.getTransaction().begin();
+		
+//		Conta conta = manager.find(Conta.class, 1L);
+		
+		Conta conta = manager.getReference(Conta.class, 1L);
+		
+		//ambos colocam o objeto no estado MANAGED.
+	
+		System.out.println(conta.toString());
+		
+		conta.setTitular("Lucas Moreira 2");
+		
+		manager.getTransaction().commit();
+		
+		manager.close();
+		
+	}
+}
